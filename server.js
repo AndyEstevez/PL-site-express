b3f10f9574dd823f0ef1d328846f59a5c6aec0d7
unknown <andyestevez27@gmail.com>
2020-12-31 00:50:13 -0500

    heroku testing

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 // const axios = require('axios');
+const path = require('path');
 const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
 app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
 // API routes
 app.use('/api', require('./routes/api-calls'));
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 const port = 5000;
 
-app.listen(port, () => console.log(`Server started on ${port}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server started on ${port}`));
