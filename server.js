2df1d7d537af14bbf14a7893168f6e48cf62a240
unknown <andyestevez27@gmail.com>
2020-12-31 00:54:32 -0500

    forgot to fix port

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.get('*', (req, res) => {
   });
 const port = 5000;
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server started on ${port}`));
+app.listen(port, () => console.log(`Server started on ${port}`));
