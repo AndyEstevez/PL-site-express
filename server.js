9cdaeaae28820b75174ccc8c586f3cea8612464f
unknown <andyestevez27@gmail.com>
2020-12-31 21:28:47 -0500

    testing

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
 // API routes
 app.use('/api', require('./routes/api-calls'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
-const port = process.env.PORT;
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
+const port = 5000;
 
-app.listen(port, () => console.log(`Server started on ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`Server started on ${port}`));
